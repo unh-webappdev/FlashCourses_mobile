3b497887fdada8b211c7b5ee5eed7ce066ada4a5
unh-webappdev <37126313+unh-webappdev@users.noreply.github.com>
2018-04-17 18:07:50 -0400

    Adding sample template files.

---
+++ docs/sample.py
@@ -0,0 +1,27 @@
+"""
+Author: Firstname Lastname
+Last Updated: Month Year
+Relative File Path: /filepath
+Description: Self-explanatory, be sure to use active verbs.
+"""
+
+#function definition 
+def some_function():
+
+	"""Explanation of function
+
+	arguments:
+	returns:
+	"""
+
+#class definition 
+class some_class():
+
+	"""Explanation of class"""
+	
+	def class_specific_function():
+		"""
+		arguments:
+		returns:
+		"""
+	"""
