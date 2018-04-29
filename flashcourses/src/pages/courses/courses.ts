9aca28186353f381ad1fc8852f7aca39c32793e8
ndb1995 <ndbielinski@gmail.com>
2018-04-29 14:20:41 -0400

    Added correct courses being shown, courses filtering and added my info to my pages

--- flashcourses/src/pages/courses/courses.ts
+++ flashcourses/src/pages/courses/courses.ts
@@ -1,7 +1,13 @@
+/*
+Author: Nicholas Bielinski
+Last Modified: 04/29/2018
+path:"/src/pages/courses/courses.ts"
+*/
+
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ApiProvider } from '../../providers/api/api';
-import { DecksPage } from '../decks/decks'
+import { CourseModel } from './courseModel';
 
 /**
  * Generated class for the CoursesPage page.
@@ -18,21 +24,19 @@ import { DecksPage } from '../decks/decks'
 export class CoursesPage {
   CoursesList:String[];
   institution:String;
+  courses: CourseModel[];
+
 
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private api_service:ApiProvider) {
-    // when the page loads, load data
-    this.initializeCourses();
-    // get insitution from other page
-    this.institution = navParams.get('data');
-
+      // when the page loads, load data
+      this.initializeCourses();
+      // get institution from other page
+      this.institution = navParams.get('data');
   }
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad CoursesPage');
-  }
-
-  toDecks(course_unique_id) {
-      this.navCtrl.push(DecksPage, {course_unique_id: course_unique_id});
   }
 
   // this grabs the data from the API
@@ -41,8 +45,20 @@ export class CoursesPage {
   // all course objects from that school
   initializeCourses(){
     this.api_service.getGetObject("/courses/api/institution/detail/" + this.navParams.get('data'),{})
-    .subscribe(courses =>{this.CoursesList = courses
-    });
+    .subscribe(_courses => {this.courses = _courses.courses});
+    
+  }
+
+  getItems(ev: any) {
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.courses = this.courses.filter((course) => {
+        return (course.course_title.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 
 }
