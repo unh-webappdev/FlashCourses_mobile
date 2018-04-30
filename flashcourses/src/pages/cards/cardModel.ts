20b5ebda2674235d1a4c5e4ab46add1633b3f07c
raghava <araghava92@gmail.com>
2018-04-29 21:38:07 -0400

    implements cycling through cards and removes courses and decks buttons from bottom nav

--- flashcourses/src/pages/cards/cardModel.ts
+++ flashcourses/src/pages/cards/cardModel.ts
@@ -3,6 +3,7 @@ Author: Raghava Adusumilli
 Last Modified: 04/29/2018
 path:"/src/pages/cards/cardModel.ts"
 */
+
 export class CardModel{
     unique_id:string;
     parent_deck:string;
--- flashcourses/src/pages/cards/cards.module.ts
+++ flashcourses/src/pages/cards/cards.module.ts
@@ -1,3 +1,9 @@
+/*
+Author: Raghava Adusumilli
+Last Modified: 04/29/2018
+path:"/src/pages/cards/cards.module.ts"
+*/
+
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { CardsPage } from './cards';
